841604c749a5c66c80d6053449cf41b2bd7dd192
Pete Slater <peteslater@gmail.com>
2017-05-12 17:16:18 +0100

    Added volume mounting for IDEA config files

--- Idea/idea.sh
+++ Idea/idea.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xhost local:root
-sudo docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -it peteslater/idea
+sudo docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v ~/.IdeaIC2017.1:/root/.IdeaIC2017.1 -it peteslater/idea
